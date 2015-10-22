8cadbf09acc4d4d0b2e860e2050160951d0e6e06
Yann Mahe <yann@datadoghq.com>
2015-10-22 16:29:57 -0400

    [wmi] new faster WMI Python package

---
+++ checks/libs/wmi.py
@@ -0,0 +1,282 @@
+# stdlib
+import pywintypes
+from win32com.client import Dispatch
+
+# project
+
+
+class CaseInsensitiveDict(dict):
+    def __setitem__(self, key, value):
+        super(CaseInsensitiveDict, self).__setitem__(key.lower(), value)
+
+    def __getitem__(self, key):
+        return super(CaseInsensitiveDict, self).__getitem__(key.lower())
+
+    def __contains__(self, key):
+        return super(CaseInsensitiveDict, self).__contains__(key.lower())
+
+    def get(self, key):
+        return super(CaseInsensitiveDict, self).get(key.lower())
+
+
+class InvalidWMIQuery(Exception):
+    """
+    Invalid WMI Query.
+    """
+    pass
+
+
+class MissingTagBy(Exception):
+    """
+    WMI query returned multiple rows but no `tag_by` value was given.
+    """
+    pass
+
+
+class WMISampler(object):
+    """
+    A fast Python WMI module.
+
+    Headlines:
+    * Built directly on top of the pywin32 extensions
+    * Compatible with Raw and Formatted Performance Data classes
+        * Hold the previous/current sample taken from Raw Performance classes to compute new values.
+    *
+
+    Original discussion thread: https://github.com/DataDog/dd-agent/issues/1952
+    Credits to @TheCloudlessSky (https://github.com/TheCloudlessSky)
+    """
+    _wmi_locators = {}
+    _wmi_connections = {}
+
+    def __init__(self, logger, class_name, property_names, host="localhost",
+                 namespace="root\\cimv2", username="", password=""):
+        self.logger = logger
+
+        # Connection information
+        self.host = host
+        self.namespace = namespace
+        self.username = username
+        self.password = password
+
+        self.is_raw_perf_class = "_PERFRAWDATA_" in class_name.upper()
+
+        # WMI class and properties
+        # Include required properties for making calculations with raw
+        # performance counters:
+        # https://msdn.microsoft.com/en-us/library/aa394299(v=vs.85).aspx
+        # FIXME
+        # if self.is_raw_perf_class:
+        #     property_names.extend([
+        #         "Timestamp_Sys100NS",
+        #         "Frequency_Sys100NS",
+        #         # IMPORTANT: To improve performance and since they're currently
+        #         # not needed, do not include the other Timestamp/Frequency
+        #         # properties:
+        #         #   - Timestamp_PerfTime
+        #         #   - Timestamp_Object
+        #         #   - Frequency_PerfTime
+        #         #   - Frequency_Object"
+        #     ])
+        self.class_name = class_name
+        self.property_names = property_names
+
+        # Samples
+        self.current_sample = None
+        self.previous_sample = None
+        self.property_counter_types = None
+
+    def sample(self):
+        """
+        Compute new samples.
+        """
+        # if self.is_raw_perf_class and not self.previous_sample:
+        #     self.logger.debug(u"Querying for initial sample for raw performance counter.")
+        #     self.current_sample = self._query(self.class_name, self.property_names)
+        self.previous_sample = self.current_sample
+
+        self.current_sample = self._query()
+
+        self.logger.debug(u"Sample: {0}".format(self.current_sample))
+
+    def __len__(self):
+        """
+        Return the number of WMI Objects in the current sample.
+        """
+        return len(self.current_sample)
+
+    def __iter__(self):
+        """
+        Iterate on the current sample's WMI Objects.
+        """
+        for wmi_obj in self.current_sample:
+            yield wmi_obj
+
+    def __eq__(self, other):
+        """
+        Equality operator is based on the current sample.
+        """
+        return self.current_sample == other
+
+    def __str__(self):
+        """
+        Stringify the current sample's WMI Objects.
+        """
+        return str(self.current_sample)
+
+    def _get_connection(self):
+        """
+        Create and cache WMI connections.
+        """
+        connection_key = "{host}:{namespace}:{username}".format(
+            host=self.host,
+            namespace=self.namespace,
+            username=self.username
+        )
+
+        if connection_key in self._wmi_connections:
+            self.logger.debug(
+                u"Using cached connection "
+                u"(host={host}, namespace={namespace}, username={username}).".format(
+                    host=self.host,
+                    namespace=self.namespace,
+                    username=self.username
+                )
+            )
+            return self._wmi_connections[connection_key]
+
+        self.logger.debug(
+            u"Connecting to WMI server "
+            u"(host={host}, namespace={namespace}, username={username}).".format(
+                host=self.host,
+                namespace=self.namespace,
+                username=self.username
+            )
+        )
+
+        locator = Dispatch("WbemScripting.SWbemLocator")
+        self._wmi_locators[connection_key] = locator
+
+        connection = locator.ConnectServer(self.host, self.namespace, self.username, self.password)
+        self._wmi_connections[connection_key] = connection
+
+        return connection
+
+    @staticmethod
+    def _raise_for_invalid(result, tag_by):
+        """
+        FIXME: Remove me ?
+
+        Raise:
+        * `InvalidWMIQuery`: when the result returned by the WMI query is invalid
+        * `MissingTagBy`: when the result returned by tge WMI query contains multiple rows
+            but no `tag_by` value was given
+        """
+        try:
+            if len(result) > 1 and not tag_by:
+                raise MissingTagBy
+        except (pywintypes.com_error):
+            raise InvalidWMIQuery
+
+    def _query(self):
+        """
+        Query WMI using WMI Query Language (WQL) & parse the results.
+
+        Returns: List of WMI objects
+        """
+        formated_property_names = ",".join(self.property_names)
+        wql = "Select {property_names} from {class_name}".format(
+            property_names=formated_property_names,
+            class_name=self.class_name
+        )
+        self.logger.debug(u"Querying WMI: {0}".format(wql))
+
+        try:
+            # From: https://msdn.microsoft.com/en-us/library/aa393866(v=vs.85).aspx
+            flag_return_immediately = 0x10  # Default flag.
+            flag_forward_only = 0x20
+            flag_use_amended_qualifiers = 0x20000
+
+            query_flags = flag_return_immediately | flag_forward_only
+
+            # For the first query, cache the qualifiers to determine each
+            # propertie's "CounterType"
+            # FIXME
+            # if self.property_counter_types is None:
+            #     self.property_counter_types = CaseInsensitiveDict()
+            #     query_flags |= flag_use_amended_qualifiers
+
+            raw_results = self._get_connection().ExecQuery(wql, "WQL", query_flags)
+            results = self._parse_results(raw_results, includes_qualifiers=False)
+
+        except pywintypes.com_error as ex:
+            self.logger.warning(u"Failed to execute WMI query (%s)", wql, exc_info=True)
+            results = []
+
+        return results
+
+    def _parse_results(self, raw_results, includes_qualifiers):
+        """
+        Parse WMI query results in a more comprehensive form.
+
+        Returns: List of WMI objects
+        ```
+        [
+            {
+                'freemegabytes': 19742.0,
+                'name': 'C:',
+                'avgdiskbytesperwrite': 1536.0
+            }, {
+                'freemegabytes': 19742.0,
+                'name': 'D:',
+                'avgdiskbytesperwrite': 1536.0
+            }
+        ]
+        ```
+        """
+        results = []
+        for res in raw_results:
+            # Ensure all properties are available. Use case-insensitivity
+            # because some properties are returned with different cases.
+            item = CaseInsensitiveDict()
+            for prop_name in self.property_names:
+                item[prop_name] = None
+
+            for wmi_property in res.Properties_:
+                # IMPORTANT: To improve performance, only access the Qualifiers
+                # if the "CounterType" hasn't already been cached.
+                # should_get_qualifier_type = (
+                #     includes_qualifiers and
+                #     wmi_property.Name not in self.property_counter_types
+                # )
+
+                # if should_get_qualifier_type:
+
+                #     # Can't index into "Qualifiers_" for keys that don't exist
+                #     # without getting an exception.
+                #     qualifiers = {q.Name: q.Value for q in wmi_property.Qualifiers_}
+
+                #     # Some properties like "Name" and "Timestamp_Sys100NS" do
+                #     # not have a "CounterType" (since they're not a counter).
+                #     # Therefore, they're ignored.
+                #     if "CounterType" in qualifiers:
+                #         counter_type = qualifiers["CounterType"]
+                #         self.property_counter_types[wmi_property.Name] = counter_type
+
+                #         self.logger.debug(u"Caching property qualifier CounterType: %s.%s = %s",
+                #             self.class_name,
+                #             wmi_property.Name,
+                #             counter_type
+                #         )
+                #     else:
+                #         self.logger.debug(u"CounterType qualifier not found for %s.%s",
+                #             self.class_name,
+                #             wmi_property.Name
+                #         )
+
+                try:
+                    item[wmi_property.Name] = float(wmi_property.Value)
+                except ValueError:
+                    item[wmi_property.Name] = wmi_property.Value
+            results.append(item)
+        return results
---
+++ tests/core/test_wmi.py
@@ -0,0 +1,276 @@
+# stdlib\
+from functools import partial
+from itertools import count
+import logging
+import unittest
+
+# 3rd
+from mock import Mock
+
+# project
+from tests.checks.common import Fixtures
+
+
+log = logging.getLogger(__name__)
+
+WMISampler = None
+
+
+# Thoughts
+# Log WMI activity
+# Mechanism to timeout
+# Check when pywintypes.com_error are raised
+
+
+def load_fixture(f, args):
+    """
+    Build a WMI query result from a file and given parameters.
+    """
+    properties = []
+
+    # Build from file
+    data = Fixtures.read_file(f)
+    for l in data.splitlines():
+        property_name, property_value = l.split(" ")
+        properties.append(Mock(Name=property_name, Value=property_value))
+
+    # Append extra information
+    property_name, property_value = args
+    properties.append(Mock(Name=property_name, Value=property_value))
+
+    return [Mock(Properties_=properties)]
+
+
+class MockWMIConnection(object):
+    """
+    Mocked WMI connection.
+    Save connection parameters so it can be tested.
+    """
+    def __init__(self, wmi_conn_args):
+        super(MockWMIConnection, self).__init__()
+        self._wmi_conn_args = wmi_conn_args
+        self._last_wmi_query = None
+
+    def get_conn_args(self):
+        """
+        Return parameters used to set up the WMI connection.
+        """
+        return self._wmi_conn_args
+
+    def get_last_wmi_query(self):
+        """
+        Return the last WMI query submitted via the WMI connection,
+        """
+        return self._last_wmi_query
+
+    def ExecQuery(self, wql, *args, **kwargs):
+        self._last_wmi_query = wql
+        if wql == "Select AvgDiskBytesPerWrite,FreeMegabytes "\
+                "from Win32_PerfFormattedData_PerfDisk_LogicalDisk":
+            results = load_fixture("win32_perfformatteddata_perfdisk_logicaldisk", ("Name", "C:"))
+            results += load_fixture("win32_perfformatteddata_perfdisk_logicaldisk", ("Name", "D:"))
+            return results
+
+        return []
+
+
+class Counter(object):
+    def __init__(self):
+        self.value = 0
+
+    def __iadd__(self, other):
+        self.value += other
+        return self
+
+    def __eq__(self, other):
+        return self.value == other
+
+    def __str__(self):
+        return str(self.value)
+
+    def reset(self):
+        self.value = 0
+
+
+class MockDispatch(object):
+    """
+    Mock for win32com.client Dispatch class.
+    """
+    _connect_call_count = Counter()
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def reset(cls):
+        """
+        FIXME - Dirty patch to reset `ConnectServer.call_count` to 0.
+        """
+        cls._connect_call_count.reset()
+
+    def ConnectServer(self, *args, **kwargs):
+        """
+        Return a MockWMIConnection.
+        """
+        MockDispatch._connect_call_count += 1
+        wmi_conn_args = (args, kwargs)
+        return MockWMIConnection(wmi_conn_args)
+
+    # FIXME - Return the _connect_call_count[0] directly
+    ConnectServer.call_count = _connect_call_count
+
+
+class TestUnitWMI(unittest.TestCase):
+    """
+    Unit tests for WMISampler.
+    """
+    def setUp(self):
+        """
+        Mock WMI related Python packages, so it can be tested on any environment.
+        """
+        import sys
+        global WMISampler
+
+        sys.modules['pywintypes'] = Mock()
+        sys.modules['win32com'] = Mock()
+        sys.modules['win32com.client'] = Mock(Dispatch=MockDispatch)
+
+        # FIXME
+        MockDispatch.reset()
+
+        from checks.libs import wmi
+        WMISampler = partial(wmi.WMISampler, log)
+
+    def assertWMIConnWith(self, wmi_sampler, param):
+        """
+        Helper, assert that the WMI connection was established with the right parameter and value.
+        """
+        wmi_instance = wmi_sampler._get_connection()
+        wmi_conn_args, wmi_conn_kwargs = wmi_instance.get_conn_args()
+        if isinstance(param, tuple):
+            key, value = param
+            self.assertIn(key, wmi_conn_kwargs)
+            self.assertEquals(wmi_conn_kwargs[key], value)
+        else:
+            self.assertIn(param, wmi_conn_args)
+
+    def assertWMIQuery(self, wmi_sampler, wmi_query):
+        """
+        Helper, assert that the given WMI query was submitted.
+        """
+        wmi_instance = wmi_sampler._get_connection()
+        last_wmi_query = wmi_instance.get_last_wmi_query()
+        self.assertEquals(last_wmi_query, wmi_query)
+
+    def assertWMIObject(self, wmi_obj, property_names):
+        pass
+
+    def test_raw_perf_properties(self):
+        """
+        Extend the list of properties to query for RAW Performance classes.
+        """
+        # Formatted Performance class
+        wmi_sampler = WMISampler("Win32_PerfFormattedData_PerfOS_System", ["ProcessorQueueLength"])
+        self.assertEquals(len(wmi_sampler.property_names), 1)
+
+        # Raw Performance class
+        wmi_sampler = WMISampler("Win32_PerfRawData_PerfOS_System", ["ProcessorQueueLength"])
+        # self.assertEquals(len(wmi_sampler.property_names), 3)
+
+    def test_wmi_connection(self):
+        """
+        Establish a WMI connection to the specified host/namespace, with the right credentials.
+        """
+        wmi_sampler = WMISampler(
+            "Win32_PerfRawData_PerfOS_System",
+            ["ProcessorQueueLength"],
+            "myhost",
+            "some/namespace",
+            "datadog",
+            "password"
+        )
+        wmi_conn = wmi_sampler._get_connection()
+
+        # WMI connection is cached
+        self.assertIn('myhost:some/namespace:datadog', wmi_sampler._wmi_connections)
+
+        # Connection was established with the right parameters
+        self.assertWMIConnWith(wmi_sampler, "myhost")
+        self.assertWMIConnWith(wmi_sampler, "some/namespace")
+
+    def test_wmi_connection_pooling(self):
+        """
+        Share WMI connections among WMISampler objects.
+        """
+        from win32com.client import Dispatch
+
+        wmi_sampler_1 = WMISampler("Win32_PerfRawData_PerfOS_System", ["ProcessorQueueLength"])
+        wmi_sampler_2 = WMISampler("Win32_OperatingSystem", ["TotalVisibleMemorySize"])
+        wmi_sampler_3 = WMISampler("Win32_PerfRawData_PerfOS_System", ["ProcessorQueueLength"], "myhost")  # noqa
+
+        wmi_sampler_1.sample()
+        wmi_sampler_2.sample()
+
+        self.assertEquals(Dispatch.ConnectServer.call_count, 1, Dispatch.ConnectServer.call_count)
+
+        wmi_sampler_3.sample()
+
+        self.assertEquals(Dispatch.ConnectServer.call_count, 2, Dispatch.ConnectServer.call_count)
+
+    def test_wmi_query(self):
+        """
+        Query WMI using WMI Query Language (WQL).
+        """
+        wmi_sampler = WMISampler("Win32_PerfFormattedData_PerfDisk_LogicalDisk",
+                                 ["AvgDiskBytesPerWrite", "FreeMegabytes"])
+        wmi_sampler.sample()
+
+        # Assert the WMI query
+        self.assertWMIQuery(
+            wmi_sampler,
+            "Select AvgDiskBytesPerWrite,FreeMegabytes"
+            " from Win32_PerfFormattedData_PerfDisk_LogicalDisk"
+        )
+
+    def test_wmi_parser(self):
+        """
+        Parse WMI objects from WMI query results.
+        """
+        wmi_sampler = WMISampler("Win32_PerfFormattedData_PerfDisk_LogicalDisk",
+                                 ["AvgDiskBytesPerWrite", "FreeMegabytes"])
+        wmi_sampler.sample()
+
+        # Assert `results`
+        expected_results = [
+            {
+                'freemegabytes': 19742.0,
+                'name': 'C:',
+                'avgdiskbytesperwrite': 1536.0
+            }, {
+                'freemegabytes': 19742.0,
+                'name': 'D:',
+                'avgdiskbytesperwrite': 1536.0
+            }
+        ]
+
+        self.assertEquals(wmi_sampler, expected_results, wmi_sampler)
+
+    def test_wmi_sampler_iterator(self):
+        """
+        Iterate on the WMISampler object iterates on its current sample.
+        """
+        wmi_sampler = WMISampler("Win32_PerfFormattedData_PerfDisk_LogicalDisk",
+                                 ["AvgDiskBytesPerWrite", "FreeMegabytes"])
+        wmi_sampler.sample()
+
+        self.assertEquals(len(wmi_sampler), 2)
+
+        for wmi_obj in wmi_sampler:
+            self.assertWMIObject(wmi_obj, ["AvgDiskBytesPerWrite", "FreeMegabytes", "name"])
+
+
+class TestIntegrationWMI(unittest.TestCase):
+    """
+    Integration tests for WMISampler.
+    """
+    pass
